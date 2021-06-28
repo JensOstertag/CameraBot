d2019895df84e02885bccf4f8b001c7ff75fe7ed
Jens Ostertag <49905418+JensOstertag@users.noreply.github.com>
2021-06-28 12:43:02 +0200

    Update camerabot.py

--- camerabot.py
+++ camerabot.py
@@ -108,9 +108,9 @@ control_window = Tk()
 control_window.title("CameraBot")
 
 Button(control_window, text="Left", command=move_left).place(bordermode=INSIDE, relwidth=0.125, relheight=0.75, relx=0, rely=0.125)
-Button(control_window, text="Right", command=move_left).place(bordermode=INSIDE, relwidth=0.125, relheight=0.75, relx=0.875, rely=0.125)
-Button(control_window, text="Up", command=move_left).place(bordermode=INSIDE, relwidth=0.75, relheight=0.125, relx=0.125, rely=0)
-Button(control_window, text="Down", command=move_left).place(bordermode=INSIDE, relwidth=0.75, relheight=0.125, relx=0.125, rely=0.875)
+Button(control_window, text="Right", command=move_right).place(bordermode=INSIDE, relwidth=0.125, relheight=0.75, relx=0.875, rely=0.125)
+Button(control_window, text="Up", command=move_up).place(bordermode=INSIDE, relwidth=0.75, relheight=0.125, relx=0.125, rely=0)
+Button(control_window, text="Down", command=move_down).place(bordermode=INSIDE, relwidth=0.75, relheight=0.125, relx=0.125, rely=0.875)
 
 control_window.wm_attributes("-fullscreen", "True")
 control_window.mainloop()
